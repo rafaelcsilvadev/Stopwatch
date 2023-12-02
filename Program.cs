375bbc37390f4c7cf8e9bc74c284ae522c1870ff
Rafa Couto <rafaelcsilva016@outlook.com>
2023-12-02 18:04:47 -0300

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Runtime;
+using System.Threading;
+
+namespace Stopwatch
+{
+    static class Program
+    {
+        static void Main(string[] args)        {
+
+            Menu();
+        }
+
+        static void Menu()
+        {
+            Console.Clear();
+            Console.WriteLine("S: Segundos (10s = 10 segundos)");
+            Console.WriteLine("M: Minutos (10m = 10 minutos)");
+            Console.WriteLine("0: Sair (0s ou 0m)");
+            Console.Write("Quanto tempo deseja contar?: ");
+
+            string data = Console.ReadLine()!.ToLower();
+            char type = char.Parse(data.Substring(data.Length - 1,1));
+            int time = int.Parse(data.Substring(0, data.Length - 1));
+            int multiplier = 1;
+
+            if(type == 'm')
+            {
+                multiplier = 60;
+            }
+
+            if(time == 0)
+            {
+                Environment.Exit(0);
+            }
+
+            PreStart(time * multiplier);
+            Menu();
+        }
+
+        static void PreStart(int time)
+        {
+            Console.Clear();
+            Console.WriteLine("Ready...");
+            Thread.Sleep(1000);
+            Console.WriteLine("Set...");
+            Thread.Sleep(1000);
+            Console.WriteLine("Go...");
+            Thread.Sleep(2500);
+
+            Start(time);
+        }
+
+        static void  Start(int time)
+        {
+            int currentTime = 0;
+
+            while(currentTime != time)
+            {
+                Console.Clear();
+                currentTime++;
+                Console.WriteLine(currentTime);
+                Thread.Sleep(1000);
+            }
+
+            Console.Clear();
+            Console.WriteLine("Stopwatch finalizado");
+            Thread.Sleep(2500);
+        }
+    }
+}
